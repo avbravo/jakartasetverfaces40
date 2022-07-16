d425140a2beb8e7766ce71b03e8446fa7c673f85
avbravo <avbravo@gmail.com>
2022-07-15 20:38:20 -0500

    genera caja de texto y commandbutton

---
+++ src/main/java/com/avbravo/jakartasetverfaces40/jakartasetverfaces40/JAXRSConfiguration.java
@@ -0,0 +1,205 @@
+package com.avbravo.jakartasetverfaces40.jakartasetverfaces40;
+
+import jakarta.faces.FacesException;
+import jakarta.faces.application.Application;
+import jakarta.faces.application.NavigationHandler;
+import jakarta.faces.application.StateManager;
+import jakarta.faces.application.ViewHandler;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.convert.Converter;
+import jakarta.faces.el.MethodBinding;
+import jakarta.faces.el.PropertyResolver;
+import jakarta.faces.el.ReferenceSyntaxException;
+import jakarta.faces.el.ValueBinding;
+import jakarta.faces.el.VariableResolver;
+import jakarta.faces.event.ActionListener;
+import jakarta.faces.validator.Validator;
+import jakarta.ws.rs.ApplicationPath;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Locale;
+
+/**
+ * Configures JAX-RS for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+    @Override
+    public ActionListener getActionListener() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setActionListener(ActionListener al) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Locale getDefaultLocale() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setDefaultLocale(Locale locale) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getDefaultRenderKitId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setDefaultRenderKitId(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getMessageBundle() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setMessageBundle(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public NavigationHandler getNavigationHandler() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setNavigationHandler(NavigationHandler nh) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public PropertyResolver getPropertyResolver() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setPropertyResolver(PropertyResolver pr) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public VariableResolver getVariableResolver() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setVariableResolver(VariableResolver vr) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ViewHandler getViewHandler() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setViewHandler(ViewHandler vh) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public StateManager getStateManager() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setStateManager(StateManager sm) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void addComponent(String string, String string1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public UIComponent createComponent(String string) throws FacesException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public UIComponent createComponent(ValueBinding vb, FacesContext fc, String string) throws FacesException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Iterator<String> getComponentTypes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void addConverter(String string, String string1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void addConverter(Class<?> type, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Converter createConverter(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Converter createConverter(Class<?> type) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Iterator<String> getConverterIds() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Iterator<Class<?>> getConverterTypes() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public MethodBinding createMethodBinding(String string, Class<?>[] types) throws ReferenceSyntaxException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Iterator<Locale> getSupportedLocales() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setSupportedLocales(Collection<Locale> clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void addValidator(String string, String string1) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Validator createValidator(String string) throws FacesException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Iterator<String> getValidatorIds() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public ValueBinding createValueBinding(String string) throws ReferenceSyntaxException {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ src/main/java/com/avbravo/jakartasetverfaces40/view/DemoFacelet.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.avbravo.jakartasetverfaces40.view;
+
+import com.avbravo.jakartasetverfaces40.model.Oceano;
+import static jakarta.faces.application.StateManager.IS_BUILDING_INITIAL_STATE;
+
+import java.io.IOException;
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.View;
+import jakarta.faces.component.UIColumn;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.component.UIOutput;
+import jakarta.faces.component.html.*;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.el.ValueBinding;
+import jakarta.faces.view.facelets.Facelet;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author avbravo
+ */
+@View("/demo.xhtml")
+@ApplicationScoped
+public class DemoFacelet extends Facelet {
+
+    @Override
+    public void apply(FacesContext facesContext, UIComponent root) throws IOException {
+        if (!facesContext.getAttributes().containsKey(IS_BUILDING_INITIAL_STATE)) {
+            return;
+        }
+
+        ComponentBuilder components = new ComponentBuilder(facesContext);
+        List<UIComponent> rootChildren = root.getChildren();
+
+        UIOutput output = new UIOutput();
+
+        output.setValue("<html xmlns=\"http://www.w3.org/1999/xhtml\">");
+        rootChildren.add(output);
+
+        HtmlBody body = components.create(HtmlBody.COMPONENT_TYPE);
+        rootChildren.add(body);
+
+        HtmlForm form = components.create(HtmlForm.COMPONENT_TYPE);
+        form.setId("form");
+        body.getChildren().add(form);
+
+        HtmlOutputLabel label = components.create(HtmlOutputLabel.COMPONENT_TYPE);
+        label.setValue("Enter your name");
+        form.getChildren().add(label);
+
+        HtmlInputText message = components.create(HtmlInputText.COMPONENT_TYPE);
+        message.setId("message");
+        form.getChildren().add(message);
+
+        HtmlOutputText text = components.create(HtmlOutputText.COMPONENT_TYPE);
+        form.getChildren().add(text);
+        /**
+         * Datatable
+         */
+        HtmlDataTable dataTable = new HtmlDataTable();
+        // Get amount of columns.
+        List<Oceano> list = new ArrayList<>();
+        int columns = ((List) list.get(0)).size();
+
+        // Set columns.
+        for (int i = 0; i < columns; i++) {
+
+            // Set header (optional).
+            UIOutput header = new UIOutput();
+            header.setValue("idoceano");
+            
+
+            // Set output.
+            UIOutput output1 = new UIOutput();
+            ValueBinding myItem
+                    = FacesContext
+                            .getCurrentInstance()
+                            .getApplication()
+                            .createValueBinding("#{myItem[" + i + "]}");
+            output.setValueBinding("value", myItem);
+
+            // Set column.
+            UIColumn column = new UIColumn();
+            column.setHeader(header);
+            column.getChildren().add(output);
+
+            // Add column.
+          dataTable.getChildren().add(column);
+        }
+form.getChildren().add(dataTable);
+        /**
+         * CommandButton
+         */
+        HtmlCommandButton actionButton = components.create(HtmlCommandButton.COMPONENT_TYPE);
+        actionButton.setId("button");
+        actionButton.addActionListener(e -> text.setValue("Hi " + message.getValue()));
+        actionButton.setValue("Do action");
+        form.getChildren().add(actionButton);
+
+        output = new UIOutput();
+        output.setValue("</html>");
+        rootChildren.add(output);
+    }
+
+    private static class ComponentBuilder {
+
+        FacesContext facesContext;
+
+        ComponentBuilder(FacesContext facesContext) {
+            this.facesContext = facesContext;
+        }
+
+        @SuppressWarnings("unchecked")
+        <T> T create(String componentType) {
+            return (T) facesContext.getApplication().createComponent(facesContext, componentType, null);
+        }
+    }
+}
